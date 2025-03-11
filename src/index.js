7d7b0c8fcee733be26f57d59a961523888265781
Federico Spadaro <federicospadaro2@gmail.com>
2025-03-11 17:02:33 +0100

    [DONE] index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const moviesRoutes = require("./routes/moviesRoutes");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rotte
+app.use("/movies", moviesRoutes);
+
+// Avvio del server
+app.listen(PORT, () => {
+  console.log(`🚀 Server in esecuzione su http://localhost:${PORT}`);
+});
