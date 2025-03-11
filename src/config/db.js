c8f15997132c374ac070f7ff0a6d0bbafc981d28
Federico Spadaro <federicospadaro2@gmail.com>
2025-03-11 16:01:06 +0100

    [ADD] + [SETTED] db.js

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+// connessione al database
+
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Errore di connessione al database:", err);
+  } else {
+    console.log("Database connesso con successo!");
+  }
+});
+
+module.exports = db;
